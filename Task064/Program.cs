63ac1d3a23cd0b8c7959147dfc8e97799e8386a9
Pavel <p.a.dudarev@mail.ru>
2022-10-07 04:59:30 +0500

    Task 64

---
+++ Task064/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите N");
+
+int number = int.Parse(Console.ReadLine()!);
+
+void PrintNaturalNumbers(int maximum)
+{
+    if (maximum == 1)
+    {
+        Console.Write($"{maximum} ");
+
+
+
+    }
+    else
+    {
+        
+        Console.Write($"{maximum}, ");
+        PrintNaturalNumbers(maximum - 1);
+
+    }
+
+}
+
+PrintNaturalNumbers(number);
