67e0239d9179859e13439ccff0d9ef6a4f219dcf
Pavel <p.a.dudarev@mail.ru>
2022-10-07 05:24:10 +0500

    Task 66

---
+++ Task066/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите M и N");
+
+int start = int.Parse(Console.ReadLine()!);
+int end = int.Parse(Console.ReadLine()!);
+
+void FindSum(int start, int end, int sum)
+{   
+    if (start > end)
+    {
+        Console.WriteLine($"Сумма чисел от M до N = {sum}");
+        return;
+    }
+
+    sum = sum + start;
+    start++;
+    FindSum(start, end, sum);
+}
+
+FindSum(start,end,0);
